beda8b56e56d383bbc203a58d8cf01b8f297d887
Filip <filip.pocztowski@gmail.com>
2014-05-20 19:55:43 +0200

    Operacje arytmetyczne i warunek if
    Dodano operacje arytmetyczne, i warunki do ifa.
    Plik na wejściu przyjmuje wszystko, co jest w postaci:
    a=a+b;
    a=b/c;
    a=c-d;
    if(a==5) { a = a - b;}
    Taka sytuacja.

--- Plik .pl/test.pl
+++ Plik .pl/test.pl
@@ -9,11 +9,79 @@ readFromFile(File, Output) :-
                 read_stream_to_codes(Temp, Output),
                 close(Temp).
 
-program(Z) --> code(Za), {concat_atom([Za], Z)}.
+%program(Z) --> code(Za), {concat_atom([Za], Z)}.
+
+%tymczasowe wywolanei dla testów
+
+program(Z) --> add(Za), {concat_atom([Za], Z)}.
+
+program(Z) --> sub(Za), {concat_atom([Za], Z)}.
+
+program(Z) --> div(Za), {concat_atom([Za], Z)}.
+
+program(Z) --> mul(Za), {concat_atom([Za], Z)}.
+
+program(Z) --> inc(Za), {concat_atom([Za], Z)}.
+
+program(Z) --> dec(Za), {concat_atom([Za], Z)}.
+
+program(Z) --> if(Za), {concat_atom([Za], Z)}.
+
 
 code(Z) --> declaration(Z1), whitespace, {concat_atom([Z1], Z)}.
 code(Z) --> declaration(Z).
 code(Z) --> declaration(Z), code(Z).
+
+%operacje arytmetyczne
+%dodawanie
+
+add(Z) --> whitespace, chars(C), whitespace, "=",whitespace, chars(A),whitespace,add_op,whitespace, chars(B),whitespace,";", {concat_atom(['\nmov eax, [',A,']\nadd eax, [',B,']\nmov [',C,'], eax\n'],Z)}.
+
+sub(Z) --> whitespace, chars(C), whitespace, "=",whitespace, chars(A),whitespace,sub_op,whitespace, chars(B),whitespace,";", {concat_atom(['mov eax, [',A,']\nsub eax, [',B,']\nmov [',C,'], eax\n'],Z)}.
+
+div(Z) --> whitespace, chars(C), whitespace, "=",whitespace, chars(A),whitespace,div_op,whitespace, chars(B),whitespace,";", {concat_atom(['mov eax, [',A,']\nmov edx,0\nidiv dword [',B,']\nmov [',C,'], eax\n'],Z)}.
+
+mul(Z) --> whitespace, chars(C), whitespace, "=" , whitespace, chars(A),whitespace,mul_op,whitespace, chars(B),whitespace,";", {concat_atom(['mov eax, [',A,']\nimul dword [',B,']\nmov [',C,'], eax\n'],Z)}.
+
+inc(Z) --> whitespace, chars(A), whitespace, add_op, add_op, whitespace, {concat_atom(['mov eax, [',A,']\ninc eax'],Z)}.
+
+dec(Z) --> whitespace, chars(A), whitespace, sub_op, sub_op, whitespace, {concat_atom(['mov eax, [',A,']\ndec eax'],Z)}.
+
+%instrukcje warunkowe
+if(Z) --> "if",whitespace,"(",whitespace,if_cond(Za),whitespace,")",whitespace,"{",whitespace,exp_if(Zb),whitespace,"}", {concat_atom([Za,Zb],Z)}.
+% wyra¿enie które ma siê wykonywaæ w if {} póki co jest za³o¿eniem operacji dodawania
+%zak³adam, ¿e mo¿emy porównywaæ tylko liczby w postaci if(x>5)
+
+%if(x>5)
+if_cond(Z) --> chars(A),whitespace,cond_op_greater,whitespace,integer_number(B), {concat_atom(['mov eax, [',A,']\ncmp eax, ',B,'\njg klamra'],Z)}.
+
+%if(x<5)
+if_cond(Z) --> chars(A),whitespace,cond_op_less,whitespace,integer_number(B), {concat_atom(['mov eax, [',A,']\ncmp eax, ',B,'\njl klamra'],Z)}.
+
+%if(x==5)
+if_cond(Z) --> chars(A),whitespace,cond_op_equal_to,whitespace,integer_number(B), {concat_atom(['mov eax, [',A,']\ncmp eax, ',B,'\njz klamra\n'],Z)}.
+
+
+%operatory arytmetyczne
+add_op --> "+".
+sub_op--> "-".
+div_op --> "/".
+mul_op --> "*".
+
+%operatory warunkowe
+cond_op_greater --> ">".
+%cond_op_greater_or_equal --> ">=".
+cond_op_less --> "<".
+%cond_op_less_or_equal --> "<=".
+cond_op_equal_to --> "==".
+
+% exp to ka¿de mo¿liwe wyra¿enie, które siê moze pojawiæ, nale¿a³oby
+% zdefiniowaæ kilka(naœcie lub set) mo¿liwoœci
+%
+exp_if(Z) --> exp(Za), {concat_atom(['\n\nklamra:',Za],Z)}.
+
+exp(Z) --> add(Za), {concat_atom([Za], Z)}.
+
 
 declaration(Z) --> "int", whitespace, chars(A), whitespace, "=", whitespace, integer_number(B), whitespace,";",       {concat_atom([A,': dd ',B],Z)}.
 
@@ -46,3 +114,10 @@ char(C) --> [C1], {code_type(C1, alnum), atom_codes(C, [C1])}.
 
 %args --> [].
 %functions --> [].
+
+
+
+
+
+
+
